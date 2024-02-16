16aaa4379785539e6025f960ba9c902767eac908
xMathfreak <Mathewodueyingbo@gmail.com>
2024-02-16 14:14:58 +0100

    Timer stop when time elapsed

--- script.js
+++ script.js
@@ -3,10 +3,20 @@ const second = 1000,
 	  hour 	 = minute * 60,
 	  day	 = hour * 24;
 
-const targetDate = new Date("11:30 Feb 25, 2024").getTime(), 
+const targetDate = new Date("11:30 Feb 25, 2024").getTime(),
 x = setInterval(() => {
 	const now = new Date().getTime(),
 		 dist = targetDate - now;
+
+	if (dist <= 0) {
+		const t = document.getElementsByClassName("time");
+		for (let i=0; i < t.length; i++) {
+			t[i].style.display = "none";
+		}
+
+		document.getElementById("elapsed")
+			.style.display = "flex";
+	}
 
 	document.getElementById("days").innerText = Math.floor(dist / day);
 	document.getElementById("hours").innerText = Math.floor((dist % day) / hour);
